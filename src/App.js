25d048b7c9dbfcfc3fd4645efdeca8d0c8e241a3
blevinspercussion <blevinspercussion@gmail.com>
2022-06-15 10:07:01 -0500

    added handle functions, except for submit and delete for work and school

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React, { useState } from "react";
+import GeneralInfo from "./components/GeneralInfo";
+import EducationInfo from "./components/EducationInfo";
+import Resume from "./components/Resume";
 import "./App.css";
+import ExperienceInfo from "./components/ExperienceInfo";
 
 const App = () => {
+  // Initialize state
+
   const [fullName, setFullName] = useState("");
   const [email, setEmail] = useState("");
   const [phone, setPhone] = useState("");
@@ -18,15 +24,97 @@ const App = () => {
   const [startDate, setStartDate] = useState("");
   const [endDate, setEndDate] = useState("");
 
-  const [works] = useState("");
+  const [works, setWorks] = useState("");
+
+  // Handlers for general info
+  const handleName = (newName) => {
+    setFullName(newName);
+  };
+
+  const handleEmail = (newEmail) => {
+    setEmail(newEmail);
+  };
+
+  const handlePhone = (newPhone) => {
+    setPhone(newPhone);
+  };
+
+  // Handlers for education info
+  const handleSchoolName = (newSchoolName) => {
+    setSchoolName(newSchoolName);
+  };
+
+  const handleFieldOfStudy = (newFieldOfStudy) => {
+    setFieldOfStudy(newFieldOfStudy);
+  };
+
+  const handleDateOfGraduation = (newDateOfGraduation) => {
+    setDateOfGraduation(newDateOfGraduation);
+  };
+
+  // TODO functions for school submit and school delete
+
+  // Handlers for work experience
+  const handleWorkName = (newWorkName) => {
+    setWorkName(newWorkName);
+  };
+
+  const handlePosition = (newPosition) => {
+    setPosition(newPosition);
+  };
+
+  const handleTasks = (newTasks) => {
+    setTasks(newTasks);
+  };
+
+  const handleStartDate = (newStartDate) => {
+    setStartDate(newStartDate);
+  };
+
+  const handleEndDate = (newEndDate) => {
+    setEndDate(newEndDate);
+  };
+
+  // TODO functions for work submit and work delete
 
   return (
     <div className="App">
       <div className="cv-input">
         <h1>CV Input</h1>
+        <GeneralInfo
+          fullName={fullName}
+          email={email}
+          phone={phone}
+          handleName={handleName}
+          handleEmail={handleEmail}
+          handlePhone={handlePhone}
+        />
+        <EducationInfo
+          schoolName={schoolName}
+          fieldOfStudy={fieldOfStudy}
+          dateOfGraduation={dateOfGraduation}
+          schools={schools}
+          // handleSchoolSubmit={handleSchoolSubmit}
+          // deleteSchool={deleteSchool}
+        />
+        <ExperienceInfo
+          workName={workName}
+          position={position}
+          tasks={tasks}
+          startDate={startDate}
+          endDate={endDate}
+          handleWorkName={handleWorkName}
+          handlePosition={handlePosition}
+          handleTasks={handleTasks}
+          handleStartDate={handleStartDate}
+          handleEndDate={handleEndDate}
+          // handleWorkSubmit={handleWorkSubmit}
+          // handleWorkDelete={handleWorkDelete}
+        />
       </div>
       <div className="cv-output">
         <h1>CV Output</h1>
+        <Resume />
       </div>
     </div>
   );
---
+++ src/components/ExperienceInfo.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ExperienceInfo = () => {
+  return <div></div>;
+};
+
+export default ExperienceInfo;
